d7215d236bce00aa4e59cf789c976c6969b05043
Niraj Gandhi <ngandhi360@gmail.com>
2024-05-21 16:40:05 +1200

    map first iteration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import MultiLineChart from '$lib/charts/MultiLineChart.svelte';
+	import InteractiveMap from '$lib/charts/InteractiveMap.svelte';
 </script>
 
 <div class="w-full h-24 border-b-2 flex items-center justify-center">
@@ -7,5 +8,6 @@
 </div>
 
 <div class="flex flex-col">
-	<MultiLineChart />
+	<!-- <MultiLineChart /> -->
+	<InteractiveMap />
 </div>
