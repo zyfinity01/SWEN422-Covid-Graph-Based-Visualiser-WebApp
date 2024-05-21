54228429f99ac099129dfc7e424e8737adea3abe
Niraj Gandhi <ngandhi360@gmail.com>
2024-05-21 19:01:44 +1200

    rounding of values

--- src/lib/charts/InteractiveMap.svelte
+++ src/lib/charts/InteractiveMap.svelte
@@ -3,40 +3,59 @@
 	import { csv, json, geoNaturalEarth1, geoPath, scaleLinear, select } from 'd3';
 	import type { GeoPermissibleObjects } from 'd3';
 	import type { FeatureCollection, Geometry } from 'geojson';
-  
+	import { zoom, zoomTransform } from 'd3';
+
+	
 	let svgElement: SVGSVGElement;
-  
+	
 	interface CountryData {
 	  iso_code: string;
 	  total_cases: string;
+	  total_deaths: string;
 	  location: string;
 	}
-  
+	
 	type GeoJson = FeatureCollection<Geometry, { iso_a3: string }>;
-  
+	
 	let tooltip = '';
 	let showTooltip = false;
 	let tooltipX = 0;
 	let tooltipY = 0;
-  
+	
+
+	
+
+	function formatNumber(num: number) {
+    if (num >= 1000000) {
+        return (num / 1000000).toFixed(2) + 'M'; // Millions
+    } else if (num >= 1000) {
+        return (num / 1000).toFixed(2) + 'K'; // Thousands
+    } else {
+        return num.toString(); // Less than 1000
+    }
+
+	
+}
+
+	
 	onMount(async () => {
 	  const covidData: CountryData[] = await csv('data/owid-covid-latest.csv');
 	  const worldData = await json<GeoJson>('data/custom.geo.json');
-	  
+	
 	  if (!worldData) {
 		console.error("Failed to load world data.");
 		return;
 	  }
-  
+	
 	  const svg = select(svgElement);
 	  const projection = geoNaturalEarth1().fitSize([960, 600], worldData as GeoPermissibleObjects);
 	  const pathGenerator = geoPath().projection(projection);
-  
+	
 	  const casesMax = Math.max(...covidData.map(d => +d.total_cases));
 	  const colorScale = scaleLinear<string>()
 		.domain([0, casesMax])
 		.range(['#ccc', '#b00']);
-  
+	
 	  svg.selectAll('path')
 		.data(worldData.features)
 		.enter().append('path')
@@ -47,7 +66,7 @@
 		})
 		.on('mouseover', (event, d) => {
 		  const countryData = covidData.find(cd => cd.iso_code === d.properties.iso_a3);
-		  tooltip = countryData ? `${countryData.location}: ${countryData.total_cases} cases` : "No data";
+		  tooltip = countryData ? `${countryData.location}: ${formatNumber(countryData.total_cases)} cases, ${formatNumber(countryData.total_deaths)} deaths` : "No data";
 		  showTooltip = true;
 		  tooltipX = event.clientX;
 		  tooltipY = event.clientY;
@@ -60,12 +79,12 @@
 		  showTooltip = false;
 		});
 	});
-  </script>
-  
-  <svg bind:this={svgElement} width="960" height="600"></svg>
-  {#if showTooltip}
+	</script>
+	
+	<svg bind:this={svgElement} width="960" height="600"></svg>
+	{#if showTooltip}
 	<div style="position: absolute; left: {tooltipX}px; top: {tooltipY}px; background-color: white; padding: 5px; border: 1px solid black;">
 	  {tooltip}
 	</div>
-  {/if}
-  
+	{/if}
+	
